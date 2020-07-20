93cf2a14f884ebb94b4cf8c40ba925cc02aafc41
Lucas Buttow <lucas.buttow@possible.com>
2020-07-20 17:49:52 -0300

    project structure adjusts

--- src/app.js
+++ src/app.js
@@ -1,19 +1,14 @@
-const express = require('express');
+const app = require('express')();
+const consign = require('consign');
 
-const app = express();
-app.use(express.json());
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .then('./routes')
+  .then('./config/routes.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
-});
-
-app.get('/users', (req, res) => {
-  const users = [{ name: 'John Doe', mail: 'john@mail.com' }];
-  res.status(200).json(users);
-});
-
-app.post('/users', (req, res) => {
-  res.status(201).json(req.body);
 });
 
 module.exports = app;
